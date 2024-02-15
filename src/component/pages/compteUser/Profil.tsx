fbd70f267597d0845797ec84b6d229b44db83f8c
bayrem98 <bayremsayeh98@gmail.com>
2024-02-15 18:32:19 +0100

    update code in profil page to become edit data compte user client

--- src/component/pages/compteUser/Profil.tsx
+++ src/component/pages/compteUser/Profil.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import ListSubheader from "@mui/material/ListSubheader";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -27,12 +27,33 @@ import {
 import { Card } from "reactstrap";
 import Client from "../../../@types/Client";
 import Ouvrier from "../../../@types/Ouvrier";
-import { getClient } from "../../../actions/Client/action";
+import { editClient, getClient } from "../../../actions/Client/action";
 import { getOuvrier } from "../../../actions/Ouvrier/action";
+import {
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+} from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 const Profile = () => {
+  const [isOpened, setIsOpened] = useState<boolean>(false);
   const [client, setClient] = useState<Client | null>(null);
   const [ouvrier, setOuvrier] = useState<Ouvrier | null>(null);
+
+  const [nom, setNom] = useState<string>((client?.nom || ouvrier?.nom) ?? "");
+  const [prenom, setPrenom] = useState<string>(client?.prenom ?? "");
+  const [password, setPassword] = useState<string>(client?.password ?? "");
+  const [num_tel, setNum_Tel] = useState<string>(client?.num_tel ?? "");
+  const [adresse, setAdresse] = useState<string>(client?.adresse ?? "");
+  const [coverPath, setCoverPath] = useState<any>(client?.coverPath ?? "");
+  const [num_cin, setNum_cin] = useState<number>(client?.num_cin ?? 0);
 
   const userId = localStorage.getItem("user_id");
 
@@ -42,6 +63,65 @@ const Profile = () => {
       getOuvrier(userId, setOuvrier);
     }
   }, [userId]);
+
+  const changeCoverHandler = (event: any) => {
+    const selectedCover = event.target.files[0];
+    const formData = new FormData();
+    formData.append("file", selectedCover);
+    fetch(`http://localhost:5000/upload/cover`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+        setCoverPath(`http://localhost:5000/upload/cover/${result.filename}`);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setCoverPath(undefined);
+      });
+  };
+
+  const submit = () => {
+    const newClient = {
+      _id: client?._id,
+      nom,
+      prenom,
+      password,
+      num_tel,
+      adresse,
+      coverPath,
+      num_cin,
+    };
+    editClient(newClient, () => {
+      window.location.reload();
+      setIsOpened(false);
+    });
+  };
+
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const handleNomChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNom(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      submit();
+    }
+  };
+
+  const eye = <FontAwesomeIcon icon={faEye} />;
+
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
 
   return (
     <>
@@ -150,54 +230,297 @@ const Profile = () => {
                   {client && (
                     <>
                       <Grid item xs={12}>
-                        Nom: {client.nom}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Nom:
+                        </span>{" "}
+                        {client.nom}
                       </Grid>
                       <Grid item xs={12}>
-                        Prénom: {client.prenom}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Prénom:
+                        </span>{" "}
+                        {client.prenom}
                       </Grid>
                       <Grid item xs={12}>
-                        Numéro de téléphone: {client.num_tel}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Numéro de téléphone:
+                        </span>{" "}
+                        {client.num_tel}
                       </Grid>
                       <Grid item xs={12}>
-                        Numéro de CIN: {client.num_cin}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Numéro de CIN:
+                        </span>{" "}
+                        {client.num_cin}
                       </Grid>
                       <Grid item xs={12}>
-                        Adresse: {client.adresse}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Adresse:
+                        </span>{" "}
+                        {client.adresse}
                       </Grid>
+                      <br />
+                      <br />
+                      <br />
+                      <Button
+                        fullWidth
+                        variant="contained"
+                        onClick={() => setIsOpened(true)}
+                      >
+                        Modifier
+                      </Button>
                     </>
                   )}
                   {ouvrier && (
                     <>
                       <Grid item xs={12}>
-                        Nom: {ouvrier.nom}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Nom:
+                        </span>{" "}
+                        {ouvrier.nom}
                       </Grid>
                       <Grid item xs={12}>
-                        Prénom: {ouvrier.prenom}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Prénom:
+                        </span>{" "}
+                        {ouvrier.prenom}
                       </Grid>
                       <Grid item xs={12}>
-                        Numéro de téléphone: {ouvrier.num_tel}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Numéro de téléphone:
+                        </span>{" "}
+                        {ouvrier.num_tel}
                       </Grid>{" "}
                       <Grid item xs={12}>
-                        Numéro de CIN: {ouvrier.num_cin}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Numéro de CIN:
+                        </span>{" "}
+                        {ouvrier.num_cin}
                       </Grid>{" "}
                       <Grid item xs={12}>
-                        Adresse: {ouvrier.adresse}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Adresse:
+                        </span>{" "}
+                        {ouvrier.adresse}
                       </Grid>
                       <Grid item xs={12}>
-                        Profession: {ouvrier.profession}
+                        <span
+                          style={{
+                            fontSize: 18,
+                            fontWeight: "bold",
+                            color: "#1976d2",
+                          }}
+                        >
+                          Profession:
+                        </span>{" "}
+                        {ouvrier.profession}
                       </Grid>
+                      <br />
+                      <br />
+                      <br />
+                      <Button
+                        fullWidth
+                        variant="contained"
+                        onClick={() => setIsOpened(true)}
+                      >
+                        Modifier
+                      </Button>
                     </>
                   )}
                 </Grid>
-                <br />
-                <Button fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-                  Modifier
-                </Button>
               </Box>
             </Box>
           </Container>
         </div>
       </div>
+      <Modal
+        centered
+        scrollable
+        isOpen={isOpened}
+        toggle={() => setIsOpened(!isOpened)}
+      >
+        <ModalHeader
+          style={{ backgroundColor: "#1976d2", color: "white" }}
+          toggle={() => setIsOpened(!isOpened)}
+        >
+          <span>Modifier</span>
+        </ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="username">Nom</Label>
+              <Input
+                value={nom}
+                id="nom"
+                name="nom"
+                type="text"
+                onChange={handleNomChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="prenom">Prénom</Label>
+              <Input
+                value={prenom}
+                id="prenom"
+                name="prenom"
+                type="text"
+                onChange={(e) => setPrenom(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Mot de passe</Label>
+              <div className="d-flex justify-content-between">
+                <Input
+                  value={password}
+                  id="password"
+                  name="password"
+                  onChange={handlePasswordChange}
+                  type={passwordShown ? "text" : "password"}
+                  onKeyPress={handleKeyPress}
+                />
+                <Button
+                  style={{
+                    color: "white",
+                    cursor: "pointer",
+                    backgroundColor: "#1976d2",
+                    border: 0,
+                  }}
+                  onClick={togglePasswordVisiblity}
+                >
+                  {eye}
+                </Button>
+              </div>
+            </FormGroup>
+            <FormGroup>
+              <Label for="num_tel">Numero de telephone</Label>
+              <Input
+                value={num_tel}
+                id="num_tel"
+                name="num_tel"
+                type="text"
+                onChange={(e) => setNum_Tel(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="adresse">Adresse</Label>
+              <Input
+                value={adresse}
+                id="adresse"
+                name="adresse"
+                type="text"
+                onChange={(e) => setAdresse(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="adresse">Importer votre image</Label>
+              <Input
+                id="coverPath"
+                name="coverPath"
+                type="file"
+                onChange={changeCoverHandler}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="num_tel">Numero de CIN</Label>
+              <Input
+                value={num_cin}
+                id="num_cin"
+                name="num_cin"
+                type="number"
+                onChange={(e) => setNum_cin(parseInt(e.target.value))}
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter
+          style={{
+            backgroundColor: "#1976d2",
+          }}
+        >
+          <Button
+            style={{
+              backgroundColor: "#ddd9d5",
+              border: 0,
+              color: "black",
+              borderRadius: 10,
+            }}
+            onClick={submit}
+          >
+            Valider
+          </Button>{" "}
+          <Button
+            style={{
+              backgroundColor: "red",
+              color: "white",
+              border: 0,
+              borderRadius: 10,
+            }}
+            onClick={() => setIsOpened(false)}
+          >
+            Annuler
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 };
